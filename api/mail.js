3cd3658f1246ed1b74f5845a109b58ef13402fb8
parthsingh <parthsingh@ghanshyamdigital.com>
2023-04-28 19:06:13 +0530

    email functionality

--- api/mail.js
+++ api/mail.js
@@ -1,6 +1,34 @@
-export default function handler(request, response) {
+import nodemailer from 'nodemailer';
+
+export default async function handler(request, response) {
     const { email } = request.body;
     console.log('email', email);
+
+    // Create a Nodemailer transporter
+    let transporter = nodemailer.createTransport({
+        host: 'smtp-mail.outlook.com', // Replace with your SMTP host
+        port: 587, // Replace with your SMTP port
+        secure: false, // false for TLS; true for SSL
+        auth: {
+            user: 'parthsingh@ghanshyamdigital.com', // Replace with your SMTP username
+            pass: 'Ashuandcow1', // Replace with your SMTP password
+        },
+    });
+
+    try {
+        await transporter.sendMail({
+            from: 'parthsingh@ghanshyamdigital.com', // Replace with your email address
+            to: email, // Replace with the recipient's email address
+            subject: 'Test Email',
+            text: 'This is a test email',
+        });
+        console.log('Email sent successfully');
+        return response.end('Email sent successfully');
+    } catch (error) {
+        console.error(error);
+    }
+
+
     return response.end(`Hello!`);
 
     // console.log(request)
